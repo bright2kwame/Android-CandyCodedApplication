a926d38d49694a9fc43171b6e1560e2797450003
bright <brightahedor@gmail.com>
2020-09-24 11:05:21 +0000

    added the dial intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -34,7 +34,10 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL, uri);
+        dialIntent.setData(uri);
+        startActivity(dialIntent);
+    }
 }
